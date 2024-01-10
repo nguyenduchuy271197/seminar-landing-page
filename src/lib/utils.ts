520c483a61e341c525eab957e090f9b91618c0fc
tuanphucnguyen2110@gmail.com <119840033+Fugena22@users.noreply.github.com>
2024-01-10 11:49:00 +0700

    fix:salary-button

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,7 @@ export function padZero(num: number) {
 export function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export function randomNumberInRange(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
