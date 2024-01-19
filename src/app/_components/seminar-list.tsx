10825405c325c91673b2165b20c02add2dea8fe4
Huy Nguyen <nguyenduchuy271197@gmail.com>
2024-01-19 17:31:02 +0700

    Update seminar-list.tsx

--- src/app/_components/seminar-list.tsx
+++ src/app/_components/seminar-list.tsx
@@ -143,7 +143,7 @@ export default function SeminarList() {
             </p>
           </div>
 
-          <ul className="grid sm:grid-cols-2 lg:grid-cols-4 gap-8">
+          <ul className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {classes.map((item, idx) => (
               <li key={idx} className="space-y-4">
                 <Link href={item.href} className="relative group">
